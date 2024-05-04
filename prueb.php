b4492b0998c5edd15f5733eaadb8c9574a6e3160
futbolparaguayotv <56748248+futbolparaguayotv@users.noreply.github.com>
2024-05-04 20:05:30 -0300

    Update prueb.php

--- prueb.php
+++ prueb.php
@@ -1,8 +1,8 @@
 <?php
 $license_json = '{
-    "payload": "https://mrpw.milcom.vcas.na1.verimatrixcloud.net:8063?deviceId=OGRhMmY4OTQtZjU0Yy0zOWI3LTgxNjUtMmYxNGZiMGNkNGQx",
+    "payload": "",
     "content_id": "Evento Star - Test",
-    "provider": "https://onetv.tigo.com/",
+    "provider": "/",
     "allowed_track_types": "SD_ONLY",
     "content_key_specs": [
         { "track_type": "SD" }
